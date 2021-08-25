4b8ecdedd96e183fd6c36e95d2be6d5fd87238e2
plabuda <25438387+plabuda@users.noreply.github.com>
2021-08-25 20:02:31 +0200

    add websockets

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-const http = require("http");
-const port = process.env.PORT || '9090';
+import { createServer } from "http";
+import WebSocket, { WebSocketServer } from "ws";
+const port = process.env.PORT || "9090";
+
+const wss = new WebSocketServer({ noServer: true, clientTracking: true });
 
 const requestListener = function (req, res) {
   console.log(req.method);
@@ -7,5 +10,20 @@ const requestListener = function (req, res) {
   res.end("Hello, World!");
 };
 
-const server = http.createServer(requestListener);
+wss.on("connection", function connection(ws) {
+  console.log("something connected");
+  ws.on("message", function incoming(message) {
+    console.log("received: %s", message);
+    ws.send("something " + message);
+  });
+
+  ws.send("something connected");
+});
+
+const server = createServer(requestListener);
+server.on("upgrade", (req, socket, head) => {
+  wss.handleUpgrade(req, socket, head, function done(ws) {
+    wss.emit("connection", ws, req);
+  });
+});
 server.listen(port);
